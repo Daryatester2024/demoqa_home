3ba5ecd27d926fd1072367694da59d8e0f88c3cd
Darya <panovadaryia@gmail.com>
2025-01-17 17:58:28 +0300

    2 commit

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -5,5 +5,7 @@ def test_check_icon (browser):
     swag_lab_page=SwagLabs(browser)
     swag_lab_page.visit()
     assert swag_lab_page.exist_icon()
+    swag_lab_page.visit()
     assert swag_lab_page.exist_user()
+    swag_lab_page.visit()
     assert swag_lab_page.exist_password()
